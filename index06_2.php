e970899f1246d717dbdd845e97f2ed0ec2de429e
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-27 20:51:40 +0200

    korecijos

---
+++ index06_2.php
@@ -0,0 +1,34 @@
+<?php
+$b = '#ffffff';
+
+_d($_GET);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $b = '#fee227';
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $b = '#d28e9c';
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background:<?= $b ?>;">
+    
+    <form action="" method="get">
+    <button type="submit" name="getas" value="lalalala">GET metodas</button>
+    </form>
+
+    <form action="" method="post">
+    <button type="submit">POST metodas</button>
+    </form>
+
+</body>
+</html>

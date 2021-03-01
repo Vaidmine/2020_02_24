4d0050b95d4279d6aab5175084f538624ee3f5ce
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-03-01 10:17:54 +0200

    korekcijos

--- nr8/pink.php
+++ nr8/pink.php
@@ -6,32 +6,28 @@ Formą nukreipkite, kad ji atsidarinėtų rose.php puslapyje. <br>
 Padarykite taip, kad surinkus naršyklėje tiesiogiai adresą į rose.php puslapį, <br> 
 naršyklė būtų peradresuojama į pink.php puslapį. 
 */
-<br> <br> <br> 
+<?php
+_d($_SERVER['REQUEST_METHOD']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Location: http://localhost/2020_02_24/nr8/rose.php');
+die;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PINK & ROSE - PINK</title>
+    <title>PINK & ROSE / this is Pink</title>
 </head>
-<body style='background-color:pink;'>
-<!-- <body style="background-color: pink; display: flex; justify-content: center;"> -->
 
-<?php
-
-
-if (isset($_POST['postas'])) {
-    header('Location:http://localhost/2020_02_24/nr8/rose.php?noredirecting=1');
-    die;
-}
-
-?>
- 
-    
-<form action="" method="post">
-<button type="text" name="postas"><p> GO TO ROSE </p></button>
-</form>
-
+<body style="background-color: pink">
+    <form action="http://localhost/2020_02_24/nr8/rose.php" method="post" >
+        <button type="submit">GO TO ROSE</button>
+    </form>
 </body>
+
 </html>
--- nr8/rose.php
+++ nr8/rose.php
@@ -1,5 +1,5 @@
 
-<?php
+
 /*
 Sukurkite du puslapius pink.php ir rose.php. Nuspalvinkite juos atitinkamo spalvom. Į
 pink.php puslapį įdėkite formą su POST metodu ir mygtuku “GO TO ROSE”. Formą
@@ -7,18 +7,12 @@ nukreipkite, kad ji atsidarinėtų rose.php puslapyje. Padarykite taip, kad suri
 naršyklėje tiesiogiai adresą į rose.php puslapį, naršyklė būtų peradresuojama į pink.php
 puslapį.
 */
-?>
-
-
-
 <?php
-if (isset($_GET['noredirecting'])) {
-    _d($_GET['noredirecting']);
-    // header ('Location: http://localhost/2020_02_24/nr8/rose.php');
-  
-} elseif (isset($_GET)) {
-    header ('Location: http://localhost/2020_02_24/nr8/pink.php');
+    _d($_SERVER['REQUEST_METHOD']);
     
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    header('Location: http://localhost/2020_02_24/nr8/pink.php');
+   die;
 }
 ?>
 
@@ -28,12 +22,9 @@ if (isset($_GET['noredirecting'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PINK & ROSE - ROSE</title>
+    <title>PINK & ROSE / this is rose </title>
 </head>
-
-<body style="background-color:MediumVioletRed;">
- 
-
+<body style="background-color: darkviolet">
 
 </body>
 </html>
